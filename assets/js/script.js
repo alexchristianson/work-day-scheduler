10230fd60a788b11782cfd97d0349cdd15e3190c
Alex C <alex.christianson.05@gmail.com>
2021-09-24 12:17:16 -0500

    change background color for past, present, future

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,32 @@ $(".saveBtn").on("click", function() {
     localStorage.setItem(hourDiv, textValue);
 });
 // repeat for each hour (change the hour)
-$("#9 .text-value").val(localStorage.getItem("9"))
+$("#9 .text-value").val(localStorage.getItem("9"));
+$("#10 .text-value").val(localStorage.getItem("10"));
+$("#11 .text-value").val(localStorage.getItem("11"));
+$("#12 .text-value").val(localStorage.getItem("12"));
+$("#13 .text-value").val(localStorage.getItem("13"));
+$("#14 .text-value").val(localStorage.getItem("14"));
+$("#15 .text-value").val(localStorage.getItem("15"));
+$("#16 .text-value").val(localStorage.getItem("16"));
+$("#17 .text-value").val(localStorage.getItem("17"));
 
+var currentTime = moment().hours();
+
+function cssUpdate() {
+    $(".divTime").each(function() {
+        var divID = parseInt($(this).attr("id")); 
+        
+        if (currentTime === divID) {
+            $(this).addClass("present")
+        }
+        else if (currentTime > divID) {
+            $(this).addClass("past")
+        }
+        else {
+            $(this).addClass("future")
+        }
+    })
+};
+
+cssUpdate();
