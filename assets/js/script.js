a0d4a0bc623588101fbbbae05f145919843a87bd
Alex C <alex.christianson.05@gmail.com>
2021-09-24 11:10:48 -0500

    add task persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-$("#currentDay").moment().format("dddd, MM/DD/YYYY")
+$("#currentDay").text(moment().format("dddd, MM/DD/YYYY"));
+
+$(".saveBtn").on("click", function() {
+    var textValue = $(this).siblings(".text-value").val();
+    var hourDiv = $(this).parent().attr("id");
+
+    localStorage.setItem(hourDiv, textValue);
+});
+// repeat for each hour (change the hour)
+$("#9 .text-value").val(localStorage.getItem("9"))
+
